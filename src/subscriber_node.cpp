0533eb6db00c1df5f376a54de884ad6aa06cfac4
Sai Charan <saicharanpurre@gmail.com>
2026-02-02 12:20:50 +0530

    Implemented ROS2 C subscriber node

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -12,7 +12,7 @@ using std::placeholders::_1;
  * 1. The constructor should name the node "subscriber_node".
  * 2. Create a subscription to topic "/chatter" with message type std_msgs::msg::String.
  * 3. The subscription callback should:
- *    - Log using RCLCPP_INFO: "I heard: 'MESSAGE'" where MESSAGE is the received data
+ * - Log using RCLCPP_INFO: "I heard: 'MESSAGE'" where MESSAGE is the received data
  */
 
 class SubscriberNode : public rclcpp::Node
@@ -22,10 +22,16 @@ public:
         : Node("subscriber_node")
     {
         // TODO: Create the subscription here
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "/chatter", 10, std::bind(&SubscriberNode::topic_callback, this, _1));
     }
 
 private:
     // TODO: Define the topic_callback function here
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
